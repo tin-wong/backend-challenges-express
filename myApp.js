e909d81aa663bd0b83d229ca58f8da9f18bafa70
Tin Wong <9789311+tin-wong@users.noreply.github.com>
2022-08-03 05:01:10 -0700

    Serve a HTML file

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 app.get('/', function(req, res) {
-    res.send(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 })
 
 
