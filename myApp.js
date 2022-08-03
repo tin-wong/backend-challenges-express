904d06e3245d50561fea495e4fbdee1a8d3947ba
Tin Wong <9789311+tin-wong@users.noreply.github.com>
2022-08-03 11:15:47 -0700

    Comment out get HTML

--- myApp.js
+++ myApp.js
@@ -1,11 +1,10 @@
 let express = require('express');
 let app = express();
-console.log(__dirname)
-app.use(express.static(__dirname + "/public"));
-app.use("/public", express.static(__dirname + "/public"));
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
-})
+// app.use(express.static(__dirname + "/public"));
+// app.use("/public", express.static(__dirname + "/public"));
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/views/index.html');
+// })
 app.get('/json', (req, res) => {
     res.json({message: "Hello json"});
 })
