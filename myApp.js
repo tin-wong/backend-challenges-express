b2109169a4df047caa09ce15985a5de532fce24b
Tin Wong <9789311+tin-wong@users.noreply.github.com>
2022-08-03 05:53:57 -0700

    Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 let express = require('express');
 let app = express();
+console.log(__dirname)
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 })
+app.use(express.static(__dirname + '/public'));
 
 
 
