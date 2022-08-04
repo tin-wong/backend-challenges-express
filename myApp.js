721aa2929c607f78a67d24a1d919945d1eb4fb75
Tin Wong <9789311+tin-wong@users.noreply.github.com>
2022-08-04 16:02:40 -0700

    Chain middleware to create a time server

--- myApp.js
+++ myApp.js
@@ -1,8 +1,7 @@
 require('dotenv').config()
 let express = require('express');
-const { METHODS } = require('http');
-const { isIP } = require('net');
 let app = express();
+
 app.use((req, res, next) => {
     console.log(req.method + " " + req.path + " - " + req.ip);
     next();
@@ -12,6 +11,7 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 })
+
 app.get('/json', (req, res) => {
     if(process.env.MESSAGE_STYLE === "uppercase"){
         res.json({message: "HELLO JSON"});
@@ -20,6 +20,12 @@ app.get('/json', (req, res) => {
     }
 })
 
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({time: req.time})
+})
 
 
 
