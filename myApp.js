1a5d321e567407c98a11023c012264a3ca2454b3
Tin Wong <9789311+tin-wong@users.noreply.github.com>
2022-08-04 22:59:41 -0700

    Get Query Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -36,7 +36,7 @@ app.get('/:word/echo', (req, res, next) => {
 
 // Get Query Parameter Input from the Client
 app.get('/name', (req, res) => {
-    res.json({name: req.query.firstname + " " + req.query.lastname});
+    res.json({name: req.query.first + " " + req.query.last});
     next();
 })
 
