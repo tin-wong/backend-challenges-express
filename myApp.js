3a08aeff38fd9d9f3e18b8db6c10c4f62cf4aec3
Tin Wong <9789311+tin-wong@users.noreply.github.com>
2022-08-05 05:46:09 -0700

    Use body-parser to Parse POST Requests

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 
 app.use((req, res, next) => {
     console.log(req.method + " " + req.path + " - " + req.ip);
@@ -8,6 +9,10 @@ app.use((req, res, next) => {
 })
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 })
@@ -39,6 +44,8 @@ app.get('/name', (req, res) => {
     res.json({name: req.query.first + " " + req.query.last});
     next();
 })
+
+
 
 
 
